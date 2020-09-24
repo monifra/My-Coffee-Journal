bc17606473ff2b65a29d1b8bb92e9da1ca9e1b1a
Monika Frankowska <monia.frankowska@gmail.com>
2020-09-24 18:36:36 +0200

    add an add coffee endpoint

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1:27017/my-coffee-journal',{
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true
 });
 
 // const validator = require('validator');
@@ -47,6 +48,95 @@ mongoose.connect('mongodb://127.0.0.1:27017/my-coffee-journal',{
 // });
 //
 //
+
+
+// const Coffee = mongoose.model('Coffee', {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     producer: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     roaster: {
+//         type: String,
+//         trim: true
+//     },
+//     origin: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     elevation: {
+//         type: Number,
+//         validate(value){
+//             if(value < 0){
+//                 throw new Error('Elevation must be a positive number');
+//             }
+//         }
+//     },
+//     fermentation: {
+//         type: String,
+//         trim: true
+//     },
+//     cultivar: {
+//         type: String,
+//         trim: true
+//     },
+//     cupNotes: {
+//         type: String,
+//         trim: true
+//     },
+//     qualityScore: {
+//         type: Number,
+//         validate(value){
+//             if(value < 0){
+//                 throw new Error('Quality Score must be a positive number');
+//             }
+//         }
+//     },
+//     price: {
+//         type: Number,
+//         validate(value){
+//             if(value < 0){
+//                 throw new Error('Price must be a positive number');
+//             }
+//         }
+//     },
+//     pickedDate : {
+//         type: Date
+//     },
+//     roastDate : {
+//         type: Date
+//     },
+//     brewDate: {
+//         type: Date
+//     },
+//     brewMethod: {
+//         type: String,
+//         trim: true
+//     },
+//     notes:{
+//         type: String,
+//         trim: true
+//     }
+// });
+//
+// const coffee = new Coffee({
+//     name: "test name",
+//     producer: "test producer",
+//     origin: "test origin"
+// });
+//
+// coffee.save().then(()=>{
+//     console.log(coffee);
+// }).catch((error)=>{
+//     console.log('Error!', error);
+// });
+
 // const me = new User({
 //     name: 'Ala',
 //     email: 'ala@pl.pl',
@@ -59,6 +149,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/my-coffee-journal',{
 // }).catch((error)=>{
 //     console.log('Error!', error);
 // });
-
 
 //To RUN /Users/monikafrankowska/mongodb/bin/mongod --dbpath=/Users/monikafrankowska/mongodb-data
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('./db/mongoose');
 
 const User = require('./models/User');
+const Coffee = require('./models/Coffee');
 
 const app = express();
 //setting port for Heroku or a normal 3000s
@@ -9,16 +10,27 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/users', (req,res)=>{
+//Users Endpoints
+app.post('/users', async(req,res)=>{
     const user = new User(req.body);
+    try{
+        await user.save();
+        res.status(201).send(user);
+    } catch(err) {
+        res.status(400).send(err);
+    }
+});
+//Coffee Endpoints
+app.post('/coffees', async(req,res)=>{
+    const coffee = new Coffee(req.body);
 
-    user.save()
-        .then(()=>{
-            res.send(user);
-        })
-        .catch((err)=>{
-            res.status(400).send(err);
-        });
+    try{
+        await coffee.save();
+        res.status(201).send(coffee);
+    }catch(err){
+        res.status(400).send(err);
+    }
+
 });
 
 app.listen(port, () => {
